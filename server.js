7515d289c9cbeb69c04f0e0d4cf584959cdefd2c
kimhanmin <cjswogksals@naver.com>
2024-11-11 15:05:50 +0900

    fix port

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongoose
         console.log(err);
     });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.listen(port,()=>{
     console.log(`앱이 ${port}번 포트에서 실행중입니다.`);
